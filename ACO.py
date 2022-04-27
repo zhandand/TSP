8559bc0b0433587fb6158ae02f16b5d5f68b4df9
zhandand <947783684@qq.com>
2022-04-27 22:24:12 +0800

    feat: class ACO

--- ACO.py
+++ ACO.py
@@ -3,30 +3,57 @@
 """
 import random
 import sys
-
+import math
 import numpy as np
 
 (city_num, ant_num) = (50, 50)
 # parameter
-(ALPHA, BETA, RHO, Q) = (1.0, 2.0, 0.5, 100.0)
-pheromone_graph = [[1.0 for _ in range(city_num)] for _ in range(city_num)]
-distance_graph = [[1.0 for _ in range(city_num)] for _ in range(city_num)]
+# (ALPHA, BETA, RHO, Q) = (1.0, 2.0, 0.5, 100.0)
+# pheromone_graph = [[1.0 for _ in range(city_num)] for _ in range(city_num)]
+# distance_graph = [[1.0 for _ in range(city_num)] for _ in range(city_num)]
 
 
 class Ant(object):
-    def __init__(self, id) -> None:
+    __ALPHA = 0.0
+    __BETA = 0.0
+    __Q = 1.0
+
+    def __init__(self, id, ALPHA, BETA, Q) -> None:
         super().__init__()
         self.id = id
+        Ant.setHyperparameter(ALPHA, BETA, Q)
         self.__initialize()
 
     def __initialize(self):
-        self.current_city = random.randint(0, city_num-1)       # 随机初始化城市
-        self.path = [self.current_city]                         # ant的轨迹
-        self.total_distance = 0.0
-        self.step = 1                                           # 移动次数
-        self.avaliable_city = [True for i in range(city_num)]   # 未访问城市
-        self.avaliable_city[self.current_city] = False
-    
+        self.__current_city = random.randint(0, city_num-1)       # 随机初始化城市
+        self.__path = [self.__current_city]                         # ant的轨迹
+        self.__total_distance = 0.0
+        self.__step = 1                                           # 移动次数
+        self.__avaliable_city = [True for i in range(city_num)]   # 未访问城市
+        self.__avaliable_city[self.__current_city] = False
+
+    @classmethod
+    def setHyperparameter(cls, alpha, beta, Q):
+        cls.__ALPHA = alpha
+        cls.__BETA = beta
+        cls.__Q = Q
+
+    @classmethod
+    def getAlpha(cls):
+        return cls.__ALPHA
+
+    @classmethod
+    def getBeta(cls):
+        return cls.__BETA
+
+    @classmethod
+    def getQ(cls):
+        """
+        Returns:
+            _type_: The pheromone released by the ant cycle once 
+        """
+        return cls.__Q
+
     def rand_choose(self, p):
         """
             Roulette for choosing city
@@ -43,45 +70,120 @@ class Ant(object):
                 break
         return i
 
-    def __choose_next_city(self):
+    def __choose_next_city(self, distance_graph, pheromone_graph):
+        """
+            choose next city according to pheromone
+        Args:
+            distance_graph (np.array[[]]): [city_num, city_num]
+            pheromone_graph (np.array[[]]): [city_num, city_num]
+        """
         select_cities_prob = [0.0 for _ in range(city_num)]
 
         for i in range(city_num):
-            if self.avaliable_city[i] is True:
+            if self.__avaliable_city[i] is True:
                 try:
                     # the probability is proportional to pheromone, inversely proportional to distance
-                    select_cities_prob[i] = pow(pheromone_graph[self.current_city][i], ALPHA) * pow(
-                        1.0/pow(distance_graph[self.current_city][i]), BETA)
+                    select_cities_prob[i] = pow(pheromone_graph[self.__current_city][i], Ant.getAlpha()) * pow(
+                        1.0/pow(distance_graph[self.__current_city][i]), Ant.getBeta())
                 except ZeroDivisionError as e:
                     print('Ant ID: {ID}, current city: {current}, target city: {target}'.format(
-                        ID=self.ID, current=self.current_city, target=i))
+                        ID=self.ID, current=self.__current_city, target=i))
                     sys.exit(1)
         select_cities_prob = np.array(select_cities_prob)
-        select_cities_prob = select_cities_prob / np.sum(select_cities_prob) # normalization
-        next_city = self.rand_choose(self,select_cities_prob)
+        select_cities_prob = select_cities_prob / \
+            np.sum(select_cities_prob)  # normalization
+        next_city = self.rand_choose(self, select_cities_prob)
         return next_city
-    
-    def __move(self, next_city):
+
+    def __move(self, next_city, distance):
         """
             ant moves to the next city
         Args:
             next_city (_type_): _description_
+            distance (_type_): the distance from current city to the next city
         """
-        self.path.append(next_city)
-        self.avaliable_city[next_city] = False
-        self.total_distance += distance_graph[self.current_city][next_city]
-        self.current_city = next_city
-        self.step +=1
-    
-    def search_food(self):
+        self.__path.append(next_city)
+        self.__avaliable_city[next_city] = False
+        self.__total_distance += distance
+        self.__current_city = next_city
+        self.__step += 1
+
+    def __release_pheromone(self):
         """
-            search one circle
+            release pheromone on the road
+        """
+        self.__pheromone = np.zeros([len(self.__path), len(self.__path)])
+        for i in range(len(self.__path)-1):
+            cur_city = self.__path[i]
+            next_city = self.__path[i+1]
+            self.__pheromone[cur_city][next_city] = Ant.getQ() / \
+                self.__total_distance
+            self.__pheromone[next_city][cur_city] = self.__pheromone[cur_city][next_city]
+        self.__pheromone[0][self.__path[-1]] = Ant.getQ() / \
+            self.__total_distance
+        self.__pheromone[self.__path[-1]][0] = Ant.getQ() / \
+            self.__total_distance
+        return self.__pheromone
+
+    def run(self, distance_graph, pheromone_graph):
+        """
+            find one circle according to pheromone
+        Args:
+            distance_graph (np.array[[]]): [city_num, city_num]
+            pheromone_graph (np.array[[]]): [city_num, city_num]
+        Returns:
+            path: the path which the ant traverses
+            total_distance: the path which the ant traverses
+            pheromone: the pheromone which the ant releases
         """
         self.__initialize()
         # search all the cities
-        while self.step < city_num:
-            next_city = self.__choose_next_city()
-            self.__move(next_city)
-        self.total_distance += distance_graph[self.path[-1]][self.path[0]]
+        while self.__step < city_num:
+            next_city = self.__choose_next_city(
+                distance_graph, pheromone_graph)
+            self.__move(
+                next_city, distance_graph[self.__current_city][next_city])
+        self.__total_distance += distance_graph[self.__path[-1]
+                                                ][self.__path[0]]
+        return self.__path, self.__total_distance, self.__release_pheromone()
 
 
+class ACO(object):
+    def __init__(self, distance_graph) -> None:
+        self.__iter = 500
+        self.__ant_num = 50   # the number of ants
+        self.__alpha = 1      # Pheromone importance factor
+        self.__beta = 2       # Important factor of heuristic function
+        self.__rho = 0.1      # Pheromone volatilization factor
+        self.__Q = 1.0
+        self.__city_num = len(distance_graph)
+        self.__pheromone_graph = np.ones(
+            [self.__city_num, self.__city_num])    # 全0初始化会在轮盘赌时出错
+
+    def generate_ants_population(self):
+        """
+            generate ants population
+        """
+        self.__ants = []
+        for i in range(self.__ant_num):
+            self.__ants.append(Ant(self.__alpha, self.__beta, self.__Q))
+
+    def run(self):
+        best_path = None
+        shortest_distance = math.inf
+        for epoch in range(self.__iter):
+            release_pheromone = []
+            for ant in self.__ants:
+                path, total_distance, pheromone = ant.run(
+                    self.__city_num, self.__pheromone_graph)
+                release_pheromone.append(pheromone)
+                if total_distance < shortest_distance:
+                    shortest_distance = total_distance
+                    best_path = path
+            # update pheromone graph
+            self.__pheromone_graph = (1-self.__rho) * self.__pheromone_graph + np.vstack(
+                release_pheromone).reshape(self.__ant_num, self.__city_num, self.__city_num).sum(axis=2)
+
+
+if __name__ == '__main__':
+    ant = Ant(1, 0.1, 0.2)
