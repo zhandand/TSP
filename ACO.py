71760dc138484e6182a66cbe32e142c1ccc6da93
zhandand <947783684@qq.com>
2022-06-20 16:43:57 +0800

    debug: change multiple line comments into single line comments

--- ACO.py
+++ ACO.py
@@ -1,11 +1,12 @@
-"""
-解决TSP问题的蚁群算法（Ant Colony Optimization）
-"""
+# Ant Colony Optimization to solve Traveling Salesman Problem
+# Contains two classes:
+#        Ant.py
+#        ACO.py
+
 import math
 import random
 import sys
-from multiprocessing import Pool
-from importlib_metadata import re
+# from multiprocessing import Pool
 
 import numpy as np
 from tqdm import tqdm
@@ -13,17 +14,16 @@ from tqdm import tqdm
 from TSPInstance import TSPInstance
 from utils import draw_quality, timer
 
-# (city_num, ant_num) = (50, 50)
-# parameter
-# (ALPHA, BETA, RHO, Q) = (1.0, 2.0, 0.5, 100.0)
-# pheromone_graph = [[1.0 for _ in range(city_num)] for _ in range(city_num)]
-# distance_graph = [[1.0 for _ in range(city_num)] for _ in range(city_num)]
-
 
 class Ant(object):
+    # Instance of ant in colony
+    # times of pheromone intensity when choosing city
     __ALPHA = 0.0
+    # times of visibility when choosing city
     __BETA = 0.0
+    # coefficient when updating pheromone intensity
     __Q = 1.0
+    # #cities
     __city_num = 0
 
     def __init__(self, city_num, ALPHA, BETA, Q):
@@ -34,19 +34,26 @@ class Ant(object):
     def __initialize(self):
         # 随机初始化城市
         self.__current_city = random.randint(
-            0, Ant.get_city_num()-1)       
+            0, Ant.get_city_num()-1)
         # ant的轨迹
-        self.__path = [self.__current_city]                         
+        self.__path = [self.__current_city]
+        # 总距离
         self.__total_distance = 0.0
         # 移动次数
-        self.__step = 1                                        
-        # 未访问城市   
+        self.__step = 1
+        # 未访问城市
         self.__avaliable_city = [
-            True for i in range(Ant.get_city_num())]  
+            True for i in range(Ant.get_city_num())]
         self.__avaliable_city[self.__current_city] = False
 
     @classmethod
     def setHyperparameter(cls, city_num, alpha, beta, Q):
+        #    set the hyperparameters for ants
+        # Args:
+        #    city_num (_type_): #cities
+        #    alpha (_type_): Pheromone importance factor
+        #    beta (_type_): Important factor of heuristic function
+        #    Q (_type_): coefficient when updating pheromone intensity
         cls.__ALPHA = alpha
         cls.__BETA = beta
         cls.__Q = Q
@@ -54,33 +61,35 @@ class Ant(object):
 
     @classmethod
     def getAlpha(cls):
+        # Returns:
+        #     _type_: Pheromone importance factor
         return cls.__ALPHA
 
     @classmethod
     def getBeta(cls):
+        # Returns:
+        #    _type_: Important factor of heuristic function
         return cls.__BETA
 
     @classmethod
     def getQ(cls):
-        """
-        Returns:
-            _type_: The pheromone released by the ant cycle once 
-        """
+        # Returns:
+        #    _type_: The pheromone released by the ant cycle once 
         return cls.__Q
 
     @classmethod
     def get_city_num(cls):
+        # Returns:
+        #    _type_: #cities
         return cls.__city_num
 
     def __rand_choose(self, p):
-        """
-            Roulette for choosing city
-        Args:
-            p (_type_): probability of choosing one city
-
-        Returns:
-            _type_: the index of the chosen city
-        """
+        #    Roulette for choosing city
+        # Args:
+        #    p (_type_): probability of choosing one city
+        #
+        # Returns:
+        #    _type_: the index of the chosen city
         x = np.random.rand()
         for i, prob in enumerate(p):
             x -= prob
@@ -89,12 +98,10 @@ class Ant(object):
         return i
 
     def __choose_next_city(self, distance_graph, pheromone_graph):
-        """
-            choose next city according to pheromone
-        Args:
-            distance_graph (np.array[[]]): [city_num, city_num]
-            pheromone_graph (np.array[[]]): [city_num, city_num]
-        """
+        #    choose next city according to pheromone
+        # Args:
+        #     distance_graph (np.array[[]]): [city_num, city_num]
+        #     pheromone_graph (np.array[[]]): [city_num, city_num]
         select_cities_prob = [0.0 for _ in range(Ant.get_city_num())]
 
         for i in range(Ant.get_city_num()):
@@ -111,17 +118,15 @@ class Ant(object):
         select_cities_prob = np.array(select_cities_prob)
         # normalization
         select_cities_prob = select_cities_prob / \
-            np.sum(select_cities_prob)  
+            np.sum(select_cities_prob)
         next_city = self.__rand_choose(select_cities_prob)
         return next_city
 
     def __move(self, next_city, distance):
-        """
-            ant moves to the next city
-        Args:
-            next_city (_type_): _description_
-            distance (_type_): the distance from current city to the next city
-        """
+        #     ant moves to the next city
+        # Args:
+        #     next_city (_type_): _description_
+        #     distance (_type_): the distance from current city to the next city
         self.__path.append(next_city)
         self.__avaliable_city[next_city] = False
         self.__total_distance += distance
@@ -129,9 +134,7 @@ class Ant(object):
         self.__step += 1
 
     def __release_pheromone(self):
-        """
-            release pheromone on the road
-        """
+        # release pheromone on the road
         pheromone = np.zeros([len(self.__path), len(self.__path)])
         for i in range(len(self.__path)-1):
             cur_city = self.__path[i]
@@ -146,16 +149,14 @@ class Ant(object):
         return pheromone
 
     def run(self, distance_graph, pheromone_graph):
-        """
-            find one circle according to pheromone
-        Args:
-            distance_graph (np.array[[]]): [city_num, city_num]
-            pheromone_graph (np.array[[]]): [city_num, city_num]
-        Returns:
-            path: the path which the ant traverses
-            total_distance: the path which the ant traverses
-            pheromone: the pheromone which the ant releases
-        """
+        #    find one circle according to pheromone
+        # Args:
+        #    distance_graph (np.array[[]]): [city_num, city_num]
+        #    pheromone_graph (np.array[[]]): [city_num, city_num]
+        # Returns:
+        #    path: the path which the ant traverses
+        #    total_distance: the path which the ant traverses
+        #    pheromone: the pheromone which the ant releases
         self.__initialize()
         # search all the cities
         while self.__step < Ant.get_city_num():
@@ -169,40 +170,38 @@ class Ant(object):
 
 
 class ACO(object):
-    def __init__(self, tspInstance) :
+    def __init__(self, tspInstance):
         self.__tspInstance = tspInstance
 
         self.__iter = 150
         # the number of ants
-        self.__ant_num = 50   
+        self.__ant_num = 50
         # Pheromone importance factor
-        self.__alpha = 2      
+        self.__alpha = 2
         # Important factor of heuristic function
-        self.__beta = 2      
-        # Pheromone volatilization factor 
-        self.__rho = 0.1      
+        self.__beta = 2
+        # Pheromone volatilization factor
+        self.__rho = 0.1
         self.__Q = 100.0
         self.__generate_ants_population()
 
         self.__city_num = len(self.__tspInstance.get_distance_graph())
         # 全0初始化会在轮盘赌时出错
         self.__pheromone_graph = np.ones(
-            [self.__city_num, self.__city_num])    
+            [self.__city_num, self.__city_num])
         # 每一轮迭代的current distance / optimal distance
         self.quality = []
         # 所有epoch中最短距离
-        self.global_opt_distance = math.inf         
+        self.global_opt_distance = math.inf
         # 所有epoch中最好的path
-        self.global_opt_path = None                
+        self.global_opt_path = None
         # 指定的epoch
-        self.epoch = []            
+        self.epoch = []
         # 指定epoch对应的tour，作画图用
-        self.paths = []             
+        self.paths = []
 
     def __generate_ants_population(self):
-        """
-            generate ants population
-        """
+        # generate ants population
         self.__ants = []
         for i in range(self.__ant_num):
             self.__ants.append(
@@ -223,20 +222,6 @@ class ACO(object):
                     shortest_distance = total_distance
                     best_path = path
 
-            # Parallel
-            # p = Pool(5)
-            # results = []
-            # for ant in self.__ants:
-            #     result = p.apply_async(ant.run, args=(
-            #         self.__tspInstance.get_distance_graph(), self.__pheromone_graph))
-            #     results.append(result)
-            # for res in results:
-            #     path, total_distance, pheromone = res.get()
-            #     release_pheromone.append(pheromone)
-            #     if total_distance < shortest_distance:
-            #         shortest_distance = total_distance
-            #         best_path = path
-
             # update pheromone graph
             self.__pheromone_graph = (1-self.__rho) * self.__pheromone_graph + np.vstack(
                 release_pheromone).reshape(self.__ant_num, self.__city_num, self.__city_num).sum(axis=0)
@@ -253,12 +238,8 @@ class ACO(object):
                 print("-"*20 + " epoch " + str(epoch) + "-"*20)
                 print('quality: {}'.format(quality))
 
-        # draw_quality(quality, './result/' +
-        #              self.__dataset + '/', self.__dataset)
-
 
 if __name__ == '__main__':
-    # datasets = ['a280', 'att48', 'berlin52']
     datasets = ['att48', 'berlin52']
     for dataset in datasets:
         print("-"*20 + dataset + "-"*20)
--- TSPInstance.py
+++ TSPInstance.py
@@ -10,14 +10,12 @@ import numpy as np
 
 class TSPInstance(object):
     def __init__(self, dirpath, datasetName):
-        """
-        约定数据集文件名为 datasetName + '.txt'
-            对应最佳结果文件名为 datasetName + '.opt.txt'
+        #约定数据集文件名为 datasetName + '.txt'
+        #    对应最佳结果文件名为 datasetName + '.opt.txt'
+        # Args:
+        #    dirpath (_type_): 数据父目录
+        #    datasetName (_type_): 数据集名字
 
-        Args:
-            dirpath (_type_): 数据父目录
-            datasetName (_type_): 数据集名字
-        """
         self.datasetName = datasetName
         self.data_name = dirpath + datasetName + '.txt'
         self.ideal_name = dirpath + datasetName + '.opt.txt'
@@ -29,12 +27,10 @@ class TSPInstance(object):
         self.plot_opt_tour()
 
     def load_dataset(self, filepath):
-        """
-        read the position of points 
-        assume thats avaliable information starts at line 6 
-        Args:
-            filepath (_type_): _description_
-        """
+        # read the position of points 
+        # assume thats avaliable information starts at line 6 
+        # Args:
+        #     filepath (_type_): _description_
         with open(filepath, "r") as f:
             data = f.readlines()
         self.x = []
@@ -53,12 +49,10 @@ class TSPInstance(object):
         assert self.x.shape == self.y.shape
 
     def load_optdata(self, filepath):
-        """
-        read the ideal ranking of points
-        assume thats avaliable information starts at line 4 
-        Args:
-            filepath (_type_): _description_
-        """
+        # read the ideal ranking of points
+        # assume thats avaliable information starts at line 4 
+        # Args:
+        #     filepath (_type_): _description_
         with open(filepath, "r") as f:
             data = f.readlines()
         self.optimaltour = []
@@ -79,41 +73,31 @@ class TSPInstance(object):
 
     @property
     def citynum(self):
-        """
-            returns the number of city
-        """
         return self.city_num
 
     def __getitem__(self, i):
-        """
-            return the coordinate of a point given index
-        Args:
-            i (_type_): index
-
-        Returns:
-            _type_: coordiante
-        """
+        #     return the coordinate of a point given index
+        # Args:
+        #     i (_type_): index
+        # Returns:
+        #     _type_: coordiante
         return (self.x[i], self.y[i])
 
     def get_distance(self, i, j):
-        """
-            calculates the Euclidean Distance of two given points
-        Args:
-            i (_type_): index of points 1
-            j (_type_): index of points 2
-        Returns:
-            the distance
-        """
+        #     calculates the Euclidean Distance of two given points
+        # Args:
+        #     i (_type_): index of points 1
+        #     j (_type_): index of points 2
+        # Returns:
+        #     the distance
         deltaX = self.x[i] - self.x[j]
         deltaY = self.y[i] - self.y[j]
         return math.sqrt(pow(deltaX, 2) + pow(deltaY, 2))
 
     def get_tour_distance(self, tour):
-        """
-            returns the whole distance of the specific tour
-        Args:
-            tour (list): _description_
-        """
+        #    returns the whole distance of the specific tour
+        # Args:
+        #     tour (list): _description_
         dis = 0.0
         for i in range(self.city_num-1):
             dis += self.distance_graph[tour[i]][tour[i+1]]
@@ -121,9 +105,7 @@ class TSPInstance(object):
         return dis
 
     def get_distance_graph(self):
-        """
-            returns the distance graph of the cities
-        """
+        #    returns the distance graph of the cities
         distance_graph = np.zeros((self.citynum, self.citynum))
         for i in range(self.citynum):
             for j in range(i+1, self.citynum):
@@ -132,17 +114,10 @@ class TSPInstance(object):
         return distance_graph
 
     def plot_opt_tour(self):
-        """
-            plot the optimal tour
-        """
         self.plot_tour(self.optimaltour, 'optimal')
 
     def plot_tour(self, tour, name):
-        """
-            draw the path of the tour
-        Args:
-            tour (list): _description_
-        """
+        #    draw the path of the tour
         # plot the dots
         for i in range(self.city_num):
             x0, y0 = self.__getitem__(i)
@@ -170,10 +145,4 @@ class TSPInstance(object):
 
 if __name__ == "__main__":
     instance = TSPInstance('dataset/', 'a280')
-    # print(instance.citynum)
     print(instance.optTourDistance)
-    # print(instance.optimaltour)
-    # instance.plot_tour(instance.optimaltour)
-    # print(instance.get_distance_graph())
-    # assert instance.get_tour_distance(
-    #     instance.optimaltour) == instance.evaluate(instance.optimaltour)
